b6cd97993c76a66c5b99d7fd0ac11bdca063edf3
sardinhub <sardindamis.smi@gmail.com>
2026-05-15 21:48:34 +0800

    Fix useRef error untuk layar TV

--- src/display/DisplayPage.jsx
+++ src/display/DisplayPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { supabase } from '../supabaseClient';
 import Header from './Header';
 import ColumnPanel from './ColumnPanel';
